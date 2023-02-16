2ca35dfe908c54578aaca0dcac6c5225bfd69cb6
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-16 22:31:57 +0100

    task4,5 correction final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+  * main - Entry point
+  *
+  *return: 0
+  */
 int main (void) 
 {
-	puts("Programming is like building a multilinguial puzzle");
+	puts("\"Programming is like building a multilinguial puzzle");
 	return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
-	printf("%s","with proper grammar, but the outcome is a piece of art,")
-	return 0
+/**
+ * main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
 }
