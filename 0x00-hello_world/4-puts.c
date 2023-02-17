e08774cbeccdf2466f0ddc74e3aea4781d16c468
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-17 02:09:46 +0100

    task4 5 final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-  * main - Entry point
-  *
-  *return: 0
-  */
-int main (void) 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilinguial puzzle");
-	return 0;
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+
