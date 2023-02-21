e43118c98c2a7d062f25cc8d1899372657cc4803
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-21 12:29:30 +0100

    task1 betty conrrection

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  *
  * Return: void
  */
-void print_alphabet(void)	
+void print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
