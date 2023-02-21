18bb67705ff431a00b435fcbff15261c2c7322ff
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-21 19:35:53 +0100

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if character is alphabetic
+ * @c: character to test
+ *
+ * Return: 1 (true) or 0
+ */
+int _isalpha(int c)
+{
+	if ('a' <= c && c >= 'c' || 'A' <= c && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
