94dede8a17ce0f7a06e757e39a25036efb2c090a
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-22 00:04:03 +0100

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int nbr;
+	int nbr2;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			nbr = i * j;
+			if (nbr < 10)
+			{
+				_putchar('0'+nbr);
+				_putchar(',');
+				_putchar('\t');
+			}
+			else
+			{
+				nbr2 = nbr % 10;
+				nbr = nbr / 10;
+				_putchar('0' + nbr);
+				_putchar('0' + nbr2);
+				_putchar(',');
+				_putchar('\t');
+			}
+		}
+		putchar('\n');
+	}
+}
