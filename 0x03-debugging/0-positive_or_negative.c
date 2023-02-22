3c4fc2ae6672a65f03af502c627efd91b647ac68
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-22 01:00:00 +0100

    task 0 try 10

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+}
