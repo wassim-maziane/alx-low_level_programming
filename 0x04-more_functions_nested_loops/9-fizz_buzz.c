c3576555f566af6a2b4e64b59363a21244cb9c18
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-23 17:17:00 +0100

    task9 try 6

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,16 +14,16 @@ int main(void)
 		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
 		else if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz ");
+		{
+			if (i != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
-		{
-			if (i < 100)
 				printf("%d ", i);
-			else
-				printf("%d", i);
-		}
 	}
 	printf("\n");
 	return (0);
