45e119bbc5bf515912bd3da2780eb171ed5095f4
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-27 15:30:23 +0100

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int cnt = 0;
+	int i;
+
+	while (*(s + cnt) > 0)
+		cnt++;
+	for (i = s + cnt - 1; i >= s; i--)
+		_putchar(*i);
+}
