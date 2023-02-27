bc986157e662c9becd6046c2cf1120e9f3024bf1
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-27 15:00:07 +0100

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int cnt = 0;
+
+	while (*(str + cnt) != '\0')
+	{
+		_putchar(*(str + cnt));
+		cnt++;
+	}
+}
