847239b96b823b0992e9bee15cc54d10fb916ffe
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-28 03:16:44 +0100

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int cnt;
+	int i;
+
+	cnt = 0;
+	while (*(str + cnt) != '\0')
+		cnt++;
+	for (i = cnt / 2; i < cnt; i++)
+		_putchar(*(str + i));
+}
