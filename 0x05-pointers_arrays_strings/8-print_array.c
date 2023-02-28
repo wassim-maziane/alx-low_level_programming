3b9d15bb15b49723b6026df6f2b705ec475dc572
MAZIANE wassim <mazianewassim@gmail.com>
2023-02-28 03:28:08 +0100

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,4 +6,14 @@
  *
  * Return: void
  */
-void print_array(int *a
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+}
