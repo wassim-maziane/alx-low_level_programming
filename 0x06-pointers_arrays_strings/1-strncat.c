3fadd38471ab79618be6d65b936b349632e49326
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-01 22:39:48 +0100

    task1 try 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,29 @@
 #include "main.h"
 /**
- *  
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to be used from src
+ *
+ * Return: pointer to string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int cnts = 0;
+	int cntd = 0;
+	int i;
+	int min = n;
+
+	while (*(src + cnts) != '\0')
+		cnts++;
+	while (*(dest + cntd) != '\0')
+		cntd++;
+	if (min > cnts)
+		min = cnts - 1;
+	for (i = 0; i <= min; i++)
+		*(dest + cntd + i) = *(src + i);
+	*(dest + cntd + min + 1) = '\0';
+	return (dest);
+}
+
+
