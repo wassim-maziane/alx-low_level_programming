4775e6d380c1b114ac98e634a65b672f656d0ab5
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-02 22:10:12 +0100

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,25 @@
  * @s1: first string
  * @s2: second string
  *
- * Return:
+ * Return: positive value for s1 > s2,negative value for s1 < s2,0 for s1 == s2
  */
 int _strcmp(char *s1, char *s2)
 {
 	int cnt = 0;
-	while (*(s1 + cnt) == *(s1 + cnt))
+	int cnt1 = 0;
+	int cnt2 = 0;
+
+	while (s1[cnt1] != '\0')
+		cnt1++;
+	while (s2[cnt2] != '\0')
+		cnt2++;
+	while (s1[cnt] == s2[cnt])
 		cnts++;
-	if *(s
+	if (cnts == cnt1 && cnts == cnt2)
+		return (0);
+	else if (s1[cnts] > s2[cnts])
+		return (1);
+	else
+		return (-1);
+}
 
