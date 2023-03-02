b397589a0ce7177b391b21664c0ca4f3b54de7f4
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-02 16:22:23 +0100

    task2 final

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,20 @@
 #include "main.h"
 /**
- * _strncpy - copies a string
- * @dest: destination address
+ * _strncpy - copies n bytes from src to dest
+ * @dest: destination string
  * @src: source string
- * @n: number of characters to take
+ * @n: number of bytes to be copeid
  *
  * Return: pointer of destination
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int cnts = 0;
-	int cntd = 0;
-	int min = n;
-	int i;
+	int i = 0;
 
-	while (*(src + cnts) != '\0')
-		cnts++;
-	while (*(dest + cntd) != '\0')
-		cntd++;
-	if ((min > cnts) && (cntd > cnts))
-		min = cnts;
-	else if ((n > cntd) && (cnts > cntd))
-		min = cntd;
-	for (i = 0; i < min; i++)
-		*(dest + i) = *(src + i);
-	if (min == n)
-		*(dest + cntd + min) = '\0';
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	return (dest);
 }
