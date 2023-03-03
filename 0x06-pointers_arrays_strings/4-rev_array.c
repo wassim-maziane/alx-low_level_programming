56b640cd4657a36c1c5a4d024e051af818a534bd
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-03 20:09:14 +0100

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverses array
+ * @a: array to be reversed
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		a[i] = c;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = a[i]
+	}
+}
