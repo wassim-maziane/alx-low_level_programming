a6f65ae82c7a75119528fb86eaef632e53b9a31b
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-06 12:10:17 +0100

    task2 try 8

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+	    char *s = "hello";
+	        char *f;
+
+		    f = _strchr(s, 'l');
+
+		        if (f != NULL)
+				    {
+					            printf("%s\n", f);
+						        }
+			    return (0);
+}
