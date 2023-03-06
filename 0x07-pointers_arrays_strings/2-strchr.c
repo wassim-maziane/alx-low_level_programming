b2234f6cc707d6e0886f4ade687cc7b59df0873e
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-06 11:32:35 +0100

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string the function will search in
+ * @c: character we will look for
+ *
+ * Return: first occurence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	while(s[i] != '\0')
+	{
+		i++;
+		if (s[i] = c)
+			return s + i;
+	}
+	return (NULL);
+}
