09067f86b5d1284d2a6079435f7dfe388575603f
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-06 12:03:39 +0100

    task2 try 7

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,10 +14,10 @@ char *_strchr(char *s, char c)
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			return ((s+i));
+			return ((s + i));
 		i++;
 	}
 	if (c == '\0')
-		return ((s+i));
+		return ((s + i));
 	return (NULL);
 }
