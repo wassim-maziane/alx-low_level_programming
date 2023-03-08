138f758c58e5bf72e05ec62bc8513e3686ed0406
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-08 15:39:43 +0100

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to string to be reversed
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if ((*s))
+	{
+		_print_rev_recursion((s + 1));
+	}
+	else
+	{
+		return;
+	}
+	_putchar(*(s));
+}
+	
+
