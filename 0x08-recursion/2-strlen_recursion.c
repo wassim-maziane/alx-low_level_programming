821d1b711ec8009a0708746c35c20686983f8874
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-08 15:48:42 +0100

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	
+
+	if (*s)
+	{
+		return 1 + _strlen_recursion((s++));
+	}
+	return 0;
+}
+	
