0254d8052a7c7eb1b5e7e3dfd30cc8193cd55c15
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-08 14:02:33 +0100

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string s, followed by a new line
+ * @s: string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+		_putchar('\0');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+}
+
+
