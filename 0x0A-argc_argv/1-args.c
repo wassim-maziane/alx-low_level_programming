02f17b29fc3702e44ced8f7da81af98a0a48c0fc
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-17 12:41:21 +0100

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: parameters
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
