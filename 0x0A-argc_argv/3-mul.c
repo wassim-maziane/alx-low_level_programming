15273f1458b5c2f6582e04b436116cfc9967424b
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-17 12:54:11 +0100

    task3 try 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - entry point
  * @argc: size of argv
@@ -13,6 +14,6 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", argv[1] * argv[2]);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
