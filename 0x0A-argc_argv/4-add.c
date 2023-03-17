6376f6736871044bfc86ba2e8f2057854f319cac
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-17 21:47:13 +0100

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: array of parameters
+ *
+ * Return: sum of given parameters
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+	int c;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (c = 0; argv[i][c]; c++)
+		{
+			if (!(isdigit(argv[i][c])))
+			{
+				printf("Error");
+				return (1);
+			}
+		}
+		sum += argv[i];
+	}
+	return (sum);
+}
