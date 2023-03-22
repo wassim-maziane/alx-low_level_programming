d228501dd0a73f6176848497148e5465701714bc
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-22 14:24:19 +0000

    task0 betty correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ar = malloc(size * sizeof(char));
-	if (ar = 0 || size <= 0)
+	if (ar == 0 || size <= 0)
 		return (0);
 	for (i = 0; i < size; i++)
 		ar[i] = c;
 	ar[size] = '\0';
 	return (ar);
 }
-	
 
