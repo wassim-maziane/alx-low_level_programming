599994ab9b099a910341ca495f340cf9e4bc9705
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-23 09:13:27 +0000

    task2 day2 try 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int cnt1 = 0;
 	int cnt2 = 0;
 	int len;
-	
+
 	if (s1 == NULL)
 		*s1 = '\0';
 	if (s2 == NULL)
@@ -27,10 +27,12 @@ char *str_concat(char *s1, char *s2)
 	if (len == 0)
 		return (NULL);
 	str = malloc((len + 1) * sizeof(char));
+	if (str == 0)
+		return (NULL);
 	str[len] = '\0';
-	while(cnt2--)
+	while (cnt2--)
 		str[cnt1 + cnt2] = s2[cnt2];
-	while(cnt1--)
+	while (cnt1--)
 		str[cnt1] = s1[cnt1];
 	return (str);
 }
