d01db114909918bf11a73f00ec3b272748f48f23
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-23 12:20:32 +0000

    task3 final try

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,10 +11,11 @@ int *array_range(int min, int max)
 {
 	int *ar;
 	int i;
+	int size;
 
-	if (max > min)
+	if (min > max)
 		return (NULL);
-	ar = malloc(sizeof(int) * (max - min + 1));
+	ar = malloc(sizeof(*ar) * (max - min + 1));
 	if (!ar)
 		return (NULL);
 	for (i = 0; i < max - min + 1; i++)
