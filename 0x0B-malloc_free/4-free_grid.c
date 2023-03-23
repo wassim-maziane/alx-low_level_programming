d0346ac3ce48009bffa07c5313d241fcae7eea19
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-23 09:47:37 +0000

    task4 betty correction

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * free_grid - frees allocated grid
+ * @grid: two dimensional grid
+ * @height: height of grid
+ *
+ * Return: void
+ */
 void free_grid(int **grid, int height)
 {
-	while(height--)
+	while (height--)
 		free(grid[height]);
 	free(grid);
 }
