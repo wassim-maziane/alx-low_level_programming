c370287299719c5c0f61fdb5d7a2d5cee22ddaf1
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-23 10:27:55 +0000

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: memory to be allocated
+ *
+ * Return: void pointer to assigned memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr,
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		exit(98);
+	}
+	return (ptr);
+}
