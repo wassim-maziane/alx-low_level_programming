2e2e2cded41bfe53b02c3deb34b436d1f04013f7
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-28 15:42:23 +0000

    task2 final correct try

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-	d->age ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+	d->age >= 0 ? printf("Age: %.6f\n", d->age) : printf("Age: (nil)\n");
 	d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 }
