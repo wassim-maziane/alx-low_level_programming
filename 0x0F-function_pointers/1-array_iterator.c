37d40ea25f50b9d28b316ac60d0cf9ca72420278
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-29 12:49:07 +0000

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as a parameter on each element
+ * of an array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function to be used
+ *
+ * Return : void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (!action)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
+
---
+++ 0x0F-function_pointers/mains.c/2-main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem(int elem)
+{
+    printf("%d\n", elem);
+}
+
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem_hex(int elem)
+{
+    printf("0x%x\n", elem);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[5] = {0, 98, 402, 1024, 4096};
+
+    array_iterator(array, 5, &print_elem);
+    array_iterator(array, 5, &print_elem_hex);
+    return (0);
+}
+julien@ubuntu:~/0x0e. Function pointers$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-array_iterator.c -o b
+julien@ubuntu:~/0x0e. Function pointers$ ./b 
+0
+98
+402
+1024
+4096
+0x0
+0x62
+0x192
+0x400
+0x1000
+julien@ubuntu:~//0x0e. Function pointers$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x0F-function_pointers
+File: 1-array_iterator.c
+     
+2. To hell with circumstances; I create opportunities
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+Write a function that searches for an integer.
+
+Prototype: int int_index(int *array, int size, int (*cmp)(int));
+where size is the number of elements in the array array
+cmp is a pointer to the function to be used to compare values
+int_index returns the index of the first element for which the cmp function does not return 0
+If no element matches, return -1
+If size <= 0, return -1
+julien@ubuntu:~/0x0e. Function pointers$ cat 2-main.c
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+    return (98 == elem);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
+{
+    return (elem > 0);
+}
+
+
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int abs_is_98(int elem)
+{
+    return (elem == 98 || -elem == 98);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+    int index;
+
+    index = int_index(array, 20, is_98);
+    printf("%d\n", index);
+    index = int_index(array, 20, abs_is_98);
+    printf("%d\n", index);
+    index = int_index(array, 20, is_strictly_positive);
+    printf("%d\n", index);
+    return (0);
+}
