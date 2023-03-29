d68afc6e535c89e31bc7d3cff01d6cfbedf5df04
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-29 12:38:16 +0000

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function that formats the printing
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
