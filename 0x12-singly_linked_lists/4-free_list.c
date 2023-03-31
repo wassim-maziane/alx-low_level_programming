9c72869adfa93fd96d476f4c779386c7659f4279
MAZIANE wassim <mazianewassim@gmail.com>
2023-03-31 03:33:09 +0000

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: list to be freed
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	if (!head)
+		return;
+	free_list(head->next);
+	if (head->str)
+		free(head->str);
+	free (head);
+}
