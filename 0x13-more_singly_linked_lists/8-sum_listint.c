1b60ec9420b387325a5ae242f3fa049526fa4782
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-04 13:58:09 +0000

    task8 documentation

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,13 +3,13 @@
  * sum_listint - sum of all data in a linked list
  * @head: head of linked list
  *
- * Return: sum 
+ * Return: sum
  */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
-	while(head)
+	while (head)
 	{
 		sum += head->n;
 		head = head->next;
