abd21556e036b501fc96f8070217ffe993128295
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-04 22:55:07 +0000

    task10 included border case

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(node);
 		return (1);
 	}
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index - 1; i++)
 	{
 		if (!(node->next))
 			return (-1);
