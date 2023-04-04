08a6e307be9a6b49bf6314a762f6ae33c403222d
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-04 13:57:17 +0000

    task8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of all data in a linked list
+ * @head: head of linked list
+ *
+ * Return: sum 
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while(head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
