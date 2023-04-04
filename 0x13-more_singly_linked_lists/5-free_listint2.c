9230b4d4676e86dd4e01aa5863cc69e54e0b795f
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-04 12:57:27 +0000

    task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-
+void free_listint(listint_t *head);
 /** free_listint2 - frees all the node in a list and reset the head to NULL.
  * @head: pointer of pointer to a list.
  */
@@ -8,13 +8,23 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (!head)
+	if (!(*head))
 		return;
 	if (*head)
 	{
 		temp = *head;
 		*head = NULL;
 		free_listint(temp);
-		return;
 	}
+}
+/**
+ * free_listint - frees a list of integers
+ * @head: head of list
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free (head);
 }
