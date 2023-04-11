7db1e9298bc55a5d90fe8d27dbc1ceb0f418226a
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-11 12:50:38 +0000

    task1 try4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!(text_content))
 	{
-	while (text_content[len])
-		len++;
+		while (text_content[len])
+			len++;
 	}
 	o = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	w = write(o, text_content, len);
