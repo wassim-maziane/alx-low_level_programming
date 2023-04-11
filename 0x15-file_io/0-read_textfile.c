b6e0f225c70b52b047433757e446b4cb66b3dda1
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-11 12:17:57 +0000

    task0 fixed indentation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,30 +8,30 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  char *buffer;
-  ssize_t o, r, w;
+	char *buffer;
+	ssize_t o, r, w;
 
-  if (!(filename))
-    return (0);
-  buffer = malloc(sizeof(char) * letters);
-  if (!(buffer))
-    return (0);
-  o = open(filename, O_RDONLY);
-  if (o == -1)
-  {
-    free(buffer);
-    return (0);
-  }
-  r = read(o, buffer, letters);
-  if (r == -1)
-  {
-    free(buffer);
-    return (0);
-  }
-  w = write(STDOUT_FILENO, buffer, letters);
-  free(buffer);
-  if (w == -1 || w != r)
-    return (0);
-  close(o);
-  return (r);
+	if (!(filename))
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!(buffer))
+		return (0);
+	o = open(filename, O_RDONLY);
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, letters);
+	free(buffer);
+	if (w == -1 || w != r)
+		return (0);
+	close(o);
+	return (r);
 }
