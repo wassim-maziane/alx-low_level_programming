181af410eeabfc5a500a8eecf08c2b779cf77f35
MAZIANE wassim <mazianewassim@gmail.com>
2023-04-11 12:21:14 +0000

    task0 try 3

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,35 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* read_textfile - reads a text file and prints it to the POSIX standard output
+* read_textfile - reads a file
 * @filename: file
-* @letters: number of letters it should read and print
+* @letters: number of letters to be read
 *
-* Return: number of letters read, 0 if failure
+* Return: number of letters or 0 if failure
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
 	ssize_t o, r, w;
+	char *buffer;
 
-	if (!(filename))
+	if (filename == NULL)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
-	if (!(buffer))
+	if (buffer == NULL)
 		return (0);
+
 	o = open(filename, O_RDONLY);
-	if (o == -1)
-	{
-		free(buffer);
-		return (0);
-	}
 	r = read(o, buffer, letters);
-	if (r == -1)
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-	w = write(STDOUT_FILENO, buffer, letters);
+
 	free(buffer);
-	if (w == -1 || w != r)
-		return (0);
 	close(o);
-	return (r);
+
+	return (w);
 }
