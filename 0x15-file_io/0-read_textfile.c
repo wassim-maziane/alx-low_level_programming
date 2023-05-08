88bf5fc3e63d35dee5867686a2b9cead22a23561
MAZIANE wassim <mazianewassim@gmail.com>
2023-05-08 11:46:01 +0100

    task0 try 3

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!(filename))
 		return (0);
-	buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * (letters));
 	if (!(buffer))
 		return (0);
 	o = open(filename, O_RDONLY);
@@ -27,5 +27,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	free(buffer);
 	close(o);
-	return (0);
+	return (w);
 }
