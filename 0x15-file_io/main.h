67816804a798f2c04b24658e372a4c5184f82dd2
MAZIANE wassim <mazianewassim@gmail.com>
2023-05-08 11:48:00 +0100

    task0 final try

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 int _putchar(int char);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
