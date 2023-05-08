d7ab4261b2ff7946e35b2da984e2c57a331857cf
MAZIANE wassim <mazianewassim@gmail.com>
2023-05-08 11:41:05 +0100

    task0 try 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
+/**
+ * read_textfile - reads from text file
+ * @filename: name of file
+ * @letters: numbers of letters to read
+ *
+ * Return: number of characters (success), 0 (failure)
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
 	char *buffer;
+
 	if (!(filename))
 		return (0);
 	buffer = malloc(sizeof(char) * (letters + 1));
@@ -12,11 +20,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 	if (o == -1 || r == -1 || w == -1 || r != w)
-	{	
+	{
 		free(buffer);
 		return (0);
 	}
-	free (buffer);
+	free(buffer);
 	close(o);
 	return (0);
 }
