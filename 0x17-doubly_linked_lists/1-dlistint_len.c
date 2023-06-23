057d96f8929112b3fc77ba06cd629e2ec7271d59
MAZIANE wassim <mazianewassim@gmail.com>
2023-06-23 16:18:32 +0100

    task1 and 2 fixed code style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,18 @@
 #include "lists.h"
+/**
+ * dlistint_len - returns length of doubly linked list
+ *
+ * @h: pointer to head
+ * Return: length of doubly linked list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t cnt = 0;
+	size_t cnt = 0;
 
-  while (h)
-  {
-    cnt++;
-    h = h -> next;
-  }
-  return (cnt);
+	while (h)
+	{
+		cnt++;
+		h = h->next;
+	}
+	return (cnt);
 }
