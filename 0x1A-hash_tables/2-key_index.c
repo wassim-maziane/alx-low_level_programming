6138edab290076fb5a43d95c293e81a3a0a2b06d
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-06 20:35:52 +0100

    task2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,12 @@
-#incdlu
+#include "hash_tables.h"
+/**
+ * key_index - gives you index of a key
+ * @key: key
+ * @size: size of the array of the hash table
+ *
+ * Return: index of key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
