8f8d37bf242587b752766aacc49d9df9d9adeddc
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-09 21:06:44 +0100

    task4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrives value of key
+ * @ht: hash_table
+ * @key: key whose value is to be retrieved
+ *
+ * Return: value associated with key or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	index =	key_index((const unsigned char *) key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
